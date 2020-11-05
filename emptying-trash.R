bf61ab5ca2844e72c8ea1b14c8c9b397c7fd1033
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-11-05 10:34:46 -0600

    Regression. No sense of hr/min.

--- emptying-trash.R
+++ emptying-trash.R
@@ -2,4 +2,14 @@ library(here)
 library(ggplot2)
 X = read.csv(here('mb_free.txt'))
 # X = read.csv('~/Desktop/emptying-trash/mb_free.txt')
-qplot(x=timenocolon, y=megabytes, data=X)
+
+lin_reg <- lm(formula = megabytes ~ timenocolon, data = X)
+m = lin_reg$coefficients[2]
+b = lin_reg$coefficients[1]
+
+mr = round(m, 1)
+eta = (250000 - b) / m
+qplot(x=timenocolon, y=megabytes, data=X) + geom_abline(slope=m, intercept=b) + labs(title=paste(mr, 'MB per min', subtitle = paste(eta, 'ETA')))
+
+# 250 = m * x + b
+# (250 - b) / m = x
