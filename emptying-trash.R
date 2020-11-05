abbd2e02a71101cb85c65d0823691626c486a6f8
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-11-05 13:05:55 -0600

    Now parsing into hrs and mins.

--- emptying-trash.R
+++ emptying-trash.R
@@ -1,15 +1,21 @@
 library(here)
 library(ggplot2)
-X = read.csv(here('mb_free.txt'))
-# X = read.csv('~/Desktop/emptying-trash/mb_free.txt')
+library(dplyr)
 
-lin_reg <- lm(formula = megabytes ~ timenocolon, data = X)
+raw = read.csv(here('mb_free.txt'))
+# raw = read.csv('~/Desktop/emptying-trash/mb_free.txt')
+
+raw %>%
+mutate(hours = timenocolon %/% 100 + (timenocolon - timenocolon %/% 100 * 100) / 60) ->
+X
+
+lin_reg <- lm(formula = megabytes ~ hours, data = X)
 m = lin_reg$coefficients[2]
 b = lin_reg$coefficients[1]
 
-mr = round(m, 1)
-eta = (250000 - b) / m
-qplot(x=timenocolon, y=megabytes, data=X) + geom_abline(slope=m, intercept=b) + labs(title=paste(mr, 'MB per min', subtitle = paste(eta, 'ETA')))
-
+mround = round(m, 1)
+eta = round((250000 - b) / m, 2)
 # 250 = m * x + b
 # (250 - b) / m = x
+
+qplot(x= hours, y=megabytes, data=X) + geom_abline(slope=m, intercept=b) + labs(title=paste(mround, 'MB per hr'), subtitle = paste(eta, 'ETA')) + xlim(10,25) + ylim(NA, 250000)
