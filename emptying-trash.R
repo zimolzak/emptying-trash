94371a0bcc8c46bcc53bea8378e9e040e5b72e06
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-11-05 22:34:28 -0600

    hline for goal, and get over 200 GB

--- emptying-trash.R
+++ emptying-trash.R
@@ -2,6 +2,8 @@ library(here)
 library(ggplot2)
 library(dplyr)
 
+goal_mebi = 231680
+
 raw = read.csv(here('mb_free.txt'))
 # raw = read.csv('~/Desktop/emptying-trash/mb_free.txt')
 
@@ -14,8 +16,8 @@ m = lin_reg$coefficients[2]
 b = lin_reg$coefficients[1]
 
 mround = round(m, 1)
-eta = round((250000 - b) / m, 2)
+eta = round((goal_mebi - b) / m, 2)
 # 250 = m * x + b
 # (250 - b) / m = x
 
-qplot(x= hours, y=megabytes, data=X) + geom_abline(slope=m, intercept=b) + labs(title=paste(mround, 'MB per hr'), subtitle = paste(eta, 'ETA')) + xlim(10,25) + ylim(NA, 250000) + geom_smooth()
+qplot(x= hours, y=megabytes, data=X) + geom_abline(slope=m, intercept=b) + labs(title=paste(mround, 'MiB per hr'), subtitle = paste(eta, 'ETA'), y = 'Available space (MiB)') + xlim(10,30) + ylim(NA, goal_mebi) + geom_smooth() + geom_hline(yintercept = goal_mebi)
